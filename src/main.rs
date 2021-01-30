b8ecb83fa1562e8ef09925649df54b8c6f526f29
choimake <choimake330@gmail.com>
2021-01-31 01:24:14 +0900

    逆ポーランドの計算ロジックをmainから分割

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
-use std::env;
-
 // 逆ポーランド記法の数式を計算するプログラム
 // - 演算子は + - * / %
 // - 数値は32bit整数
+
+use std::env;
+
+// 外部モジュールの読み込みの際には、modを使う
+mod reverse_polish;
 
 fn main() {
     // 引数を受け取る
@@ -16,88 +19,7 @@ fn main() {
     }
 
     // 計算
-    let result = reverse_polish(&mut values);
+    let result = reverse_polish::eval(&mut values);
 
     println!("calculation result is {}", result);
-}
-
-fn reverse_polish(tokens: &mut Vec<&str>) -> i32 {
-    tokens.reverse();
-
-    let mut stack: Vec<i32> = Vec::new();
-    let mut pos = 0;
-
-    while let Some(token) = tokens.pop() {
-        pos += 1;
-        if let Ok(v) = token.parse::<i32>() {
-            stack.push(v);
-        } else {
-            let rhs = stack
-                .pop()
-                .unwrap_or_else(|| panic!("invalid syntax at {}", pos));
-            let lhs = stack
-                .pop()
-                .unwrap_or_else(|| panic!("invalid syntax at {}", pos));
-
-            let result = match token {
-                "+" => lhs + rhs,
-                "-" => lhs - rhs,
-                "*" => lhs * rhs,
-                "/" => lhs / rhs,
-                "%" => lhs % rhs,
-                _ => panic!("invalid operator at {}", pos),
-            };
-            stack.push(result);
-        }
-    }
-    if stack.len() == 1 {
-        stack[0]
-    } else {
-        panic!()
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_add() {
-        let mut v: Vec<&str> = vec!["1", "2", "+"];
-        assert_eq!(reverse_polish(&mut v), 3)
-    }
-    #[test]
-    fn test_sub() {
-        let mut v: Vec<&str> = vec!["1", "2", "-"];
-        assert_eq!(reverse_polish(&mut v), -1)
-    }
-    #[test]
-    fn test_mul() {
-        let mut v: Vec<&str> = vec!["1", "2", "*"];
-        assert_eq!(reverse_polish(&mut v), 2)
-    }
-    #[test]
-    fn test_div() {
-        let mut v: Vec<&str> = vec!["5", "2", "/"];
-        assert_eq!(reverse_polish(&mut v), 2)
-    }
-    #[test]
-    fn test_mod() {
-        let mut v: Vec<&str> = vec!["3", "2", "%"];
-        assert_eq!(reverse_polish(&mut v), 1)
-    }
-
-    #[test]
-    #[should_panic]
-    fn test_invalid_operator() {
-        let mut v: Vec<&str> = vec!["1", "1", "_"];
-        assert_eq!(reverse_polish(&mut v), 1)
-    }
-
-    #[test]
-    #[should_panic]
-    fn test_fail_to_unwrap_value() {
-        let mut v: Vec<&str> = vec!["a", "1", "+"];
-        assert_eq!(reverse_polish(&mut v), 1)
-    }
 }
---
+++ src/reverse_polish.rs
@@ -0,0 +1,80 @@
+pub fn eval(tokens: &mut Vec<&str>) -> i32 {
+    tokens.reverse();
+
+    let mut stack: Vec<i32> = Vec::new();
+    let mut pos = 0;
+
+    while let Some(token) = tokens.pop() {
+        pos += 1;
+        if let Ok(v) = token.parse::<i32>() {
+            stack.push(v);
+        } else {
+            let rhs = stack
+                .pop()
+                .unwrap_or_else(|| panic!("invalid syntax at {}", pos));
+            let lhs = stack
+                .pop()
+                .unwrap_or_else(|| panic!("invalid syntax at {}", pos));
+
+            let result = match token {
+                "+" => lhs + rhs,
+                "-" => lhs - rhs,
+                "*" => lhs * rhs,
+                "/" => lhs / rhs,
+                "%" => lhs % rhs,
+                _ => panic!("invalid operator at {}", pos),
+            };
+            stack.push(result);
+        }
+    }
+    if stack.len() == 1 {
+        stack[0]
+    } else {
+        panic!()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_add() {
+        let mut v: Vec<&str> = vec!["1", "2", "+"];
+        assert_eq!(eval(&mut v), 3)
+    }
+    #[test]
+    fn test_sub() {
+        let mut v: Vec<&str> = vec!["1", "2", "-"];
+        assert_eq!(eval(&mut v), -1)
+    }
+    #[test]
+    fn test_mul() {
+        let mut v: Vec<&str> = vec!["1", "2", "*"];
+        assert_eq!(eval(&mut v), 2)
+    }
+    #[test]
+    fn test_div() {
+        let mut v: Vec<&str> = vec!["5", "2", "/"];
+        assert_eq!(eval(&mut v), 2)
+    }
+    #[test]
+    fn test_mod() {
+        let mut v: Vec<&str> = vec!["3", "2", "%"];
+        assert_eq!(eval(&mut v), 1)
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_invalid_operator() {
+        let mut v: Vec<&str> = vec!["1", "1", "_"];
+        assert_eq!(eval(&mut v), 1)
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_fail_to_unwrap_value() {
+        let mut v: Vec<&str> = vec!["a", "1", "+"];
+        assert_eq!(eval(&mut v), 1)
+    }
+}
