2fc1f0dfbc46b9bd3d231c14ec345cebba8bd691
choimake <choimake330@gmail.com>
2021-01-30 18:58:19 +0900

    コマンドの実行処理を実装

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,20 @@ use std::env;
 // - 数値は32bit整数
 
 fn main() {
+    // 引数を受け取る
     let args: Vec<String> = env::args().collect();
 
-    println!("{:?}", args);
+    // ポーランド記法に必要な引数値のみ抽出
+    let mut values: Vec<&str> = Vec::new();
+
+    for i in 1..args.len() {
+        values.push(&args[i]);
+    }
+
+    // 計算
+    let result = reverse_polish(&mut values);
+
+    println!("calculation result is {}", result);
 }
 
 fn reverse_polish(tokens: &mut Vec<&str>) -> i32 {
